79aa1e613523304801610fa67a914b48c55bb4d0
fazalata <fazal.123@hotmail.com>
2020-04-03 23:13:27 +0200

    endringer i APP og index js

--- src/router/index.js
+++ src/router/index.js
@@ -26,6 +26,15 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "AllMatretter" */ '../views/AllMatretter.vue')
   },
+
+  {
+    path: '/add-matretter',
+    name: 'AddMatretter',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "AddMatretter" */ '../views/AddMatretter.vue')
+  },
 ]
 
 const router = new VueRouter({
